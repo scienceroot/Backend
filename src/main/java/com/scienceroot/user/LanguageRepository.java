1b8ef76093efd19691c5ee582aa14eb869ae2e20
Sven Seemann <sven.seemann@bindoc.de>
2018-03-24 14:21:53 +0100

    fixed inheritance type

--- src/main/java/com/scienceroot/user/LanguageRepository.java
+++ src/main/java/com/scienceroot/user/LanguageRepository.java
@@ -15,7 +15,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface LanguageRepository extends CrudRepository<Job, Long>{
+public interface LanguageRepository extends CrudRepository<Language, Long>{
     
     @Query("SELECT lang " +
             "FROM Language lang " +

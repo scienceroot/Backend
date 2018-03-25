8e94c2ec0bb22529df970d88db322e74df5abaee
husche <husche@ping-pc>
2018-03-25 17:08:31 +0300

    hopefully fixed search

--- src/main/java/com/scienceroot/user/ApplicationUserRepository.java
+++ src/main/java/com/scienceroot/user/ApplicationUserRepository.java
@@ -14,6 +14,7 @@ public interface ApplicationUserRepository extends CrudRepository<ApplicationUse
 
     @Query("SELECT user " +
             "FROM ApplicationUser user " +
-            "WHERE lower(user.forename) like lower(concat('%', :query, '%')) or lower(user.lastname) like lower(concat('%', :query, '%')) ")
+            "WHERE lower(user.forename) like lower(concat('%', :query, '%')) or lower(user.lastname) like lower(concat('%', :query, '%')) "
+            + "or lower(' ', concat(user.firstname, ' ', user.lastname, ' ')) like lower(concat('%', :query, '%'))")
     List<ApplicationUser> search(@Param("query") String query);
 }

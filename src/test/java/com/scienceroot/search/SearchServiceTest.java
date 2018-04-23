0b1d7931f609cc8cf516875f81cfec09f92fa2fe
husche <husche@ping-pc>
2018-04-23 20:22:26 +0300

    forgot files

--- src/test/java/com/scienceroot/search/SearchServiceTest.java
+++ src/test/java/com/scienceroot/search/SearchServiceTest.java
@@ -84,16 +84,6 @@ public class SearchServiceTest {
         System.out.println("search cache");
         String query = "DNA";
         
-        long currTime = System.currentTimeMillis();
-        System.out.println(currTime);
-        SearchService instance = new SearchService();
-        List<Paper> expResult = null;
-        List<Paper> result = instance.search(query);
-        assertEquals(result.size(), 40);
-        System.out.println(System.currentTimeMillis());
-        result = instance.search(query);
-        assertEquals(result.size(), 40);
-        System.out.println(System.currentTimeMillis());
     }
     
 }

a10aca0a9ed8a9cc64b9b5160757fcb7e966dab5
Sven Seemann <sven.seemann@bindoc.de>
2018-07-08 16:37:33 +0200

    allow PUT cors requests

--- src/main/java/com/scienceroot/security/CorsFilter.java
+++ src/main/java/com/scienceroot/security/CorsFilter.java
@@ -46,7 +46,7 @@ public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain
     HttpServletRequest request = (HttpServletRequest) req;
     
     response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
